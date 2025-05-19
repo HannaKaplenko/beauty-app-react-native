a1aaf335e3ba2878550534b5931f6a16d8f2df94
Hanna <erion777a@gmail.com>
2025-05-20 00:33:53 +0300

    first commit

--- src/modules/mockScreen/index.tsx
+++ src/modules/mockScreen/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 
-
 export const MockView = () => {
 
     return (
