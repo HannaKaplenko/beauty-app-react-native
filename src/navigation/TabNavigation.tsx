d145bfbfe17c19224e73b141330cbe87389fb297
Hanna <erion777a@gmail.com>
2025-05-24 23:52:32 +0300

    first commit

--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -3,6 +3,11 @@ import { MockView } from '../modules/mockScreen';
 import { ProductsView } from '../modules/productsList';
 import { Text } from 'react-native';
 import { ProfileView } from '../modules/profile/ui';
+import { TabHomeIcon } from '../../assets/icons/TabHomeIcon';
+import { TabCartIcon } from '../../assets/icons/TabCartIcon';
+import React from 'react';
+import { TabChatIcon } from '../../assets/icons/TabChatIcon';
+import { TabProfileIcon } from '../../assets/icons/TabProfileIcon';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +18,7 @@ export const TabNavigation = () => {
         name="HomeView"
         component={ProductsView}
         options={{
+          tabBarIcon: ({ focused }) => <TabHomeIcon height={24} width={24} color={focused ? "#000" : "#888"} />,
           tabBarLabel: ({ focused }) => <Text style={{ color: focused ? "#000" : "#888" }}> Home </Text>
         }}
       />
@@ -20,6 +26,7 @@ export const TabNavigation = () => {
         name="CartView"
         component={MockView}
         options={{
+          tabBarIcon: ({ focused }) => <TabCartIcon height={24} width={24} color={focused ? "#000" : "#888"} />,
           tabBarLabel: ({ focused }) => <Text style={{ color: focused ? "#000" : "#888" }}> Cart </Text>
         }}
       />
@@ -27,6 +34,7 @@ export const TabNavigation = () => {
         name="ChatView"
         component={MockView}
         options={{
+          tabBarIcon: ({ focused }) => <TabChatIcon height={24} width={24} color={focused ? "#000" : "#888"} />,
           tabBarLabel: ({ focused }) => <Text style={{ color: focused ? "#000" : "#888" }}> Chat </Text>
         }}
       />
@@ -34,6 +42,7 @@ export const TabNavigation = () => {
         name="ProfileView"
         component={ProfileView}
         options={{
+          tabBarIcon: ({ focused }) => <TabProfileIcon height={24} width={24} color={focused ? "#000" : "#888"} />,
           tabBarLabel: ({ focused }) => <Text style={{ color: focused ? "#000" : "#888" }}> Profile </Text>
         }}
       />
