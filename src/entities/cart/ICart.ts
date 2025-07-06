900d4b4f38d3b19f34d62618a80dacf39c8215a3
Hanna <erion777a@gmail.com>
2025-07-06 23:09:15 +0300

    first commit

--- src/entities/cart/ICart.ts
+++ src/entities/cart/ICart.ts
@@ -2,10 +2,6 @@ import { IProduct } from "../product/iProduct"
 
 export interface ICart {
     items: {
-        title: ReactNode;
-        price: ReactNode;
-        description: ReactNode;
-        images: any;
         product: IProduct;
         quantity: number;
     }[]
